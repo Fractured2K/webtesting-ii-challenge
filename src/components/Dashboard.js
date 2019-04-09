f34fc192ad50e272423cd5c0f2eedd3e34d8e77b
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-09 11:42:09 -0700

    Create handleBall event handler

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,13 +14,28 @@ class Dashboard extends Component {
 		return (
 			<div>
 				<Display {...this.state} />
-				<button>Ball</button>
+				<button onClick={this.handleBall}>Ball</button>
 				<button>Strike</button>
 				<button>Foul</button>
 				<button>Hit</button>
 			</div>
 		);
 	}
+
+	handleBall = e => {
+		if (this.state.balls >= 3)
+			return this.setState({
+				balls: 0,
+				strikes: 0,
+				fouls: 0,
+				hits: 0
+			});
+
+		this.setState({
+			...this.state,
+			balls: this.state.balls + 1
+		});
+	};
 }
 
 export default Dashboard;
