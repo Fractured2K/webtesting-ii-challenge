35fc5808b593b60532fcbc3803f41f12d99693a5
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-09 11:30:59 -0700

    Create dashboard buttons

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,13 +6,18 @@ class Dashboard extends Component {
 	state = {
 		balls: 0,
 		strikes: 0,
-		fouls: 0
+		fouls: 0,
+		hits: 0
 	};
 
 	render() {
 		return (
 			<div>
 				<Display {...this.state} />
+				<button>Ball</button>
+				<button>Strike</button>
+				<button>Foul</button>
+				<button>Hit</button>
 			</div>
 		);
 	}
