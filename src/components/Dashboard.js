32268499c79987384bef05eeadae2749bb910954
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-09 11:46:14 -0700

    Create handleStrike event handler

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -15,7 +15,9 @@ class Dashboard extends Component {
 			<div>
 				<Display {...this.state} />
 				<button onClick={this.handleBall}>Ball</button>
-				<button>Strike</button>
+				<button onClick={this.handleStrike}>
+					Strike
+				</button>
 				<button>Foul</button>
 				<button>Hit</button>
 			</div>
@@ -34,6 +36,21 @@ class Dashboard extends Component {
 		this.setState({
 			...this.state,
 			balls: this.state.balls + 1
+		});
+	};
+
+	handleStrike = e => {
+		if (this.state.strikes >= 2)
+			return this.setState({
+				balls: 0,
+				strikes: 0,
+				fouls: 0,
+				hits: 0
+			});
+
+		this.setState({
+			...this.state,
+			strikes: this.state.strikes + 1
 		});
 	};
 }
