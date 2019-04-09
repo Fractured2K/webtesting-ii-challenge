9bb3d60538d6d640de81e799e59b11ddbceb586b
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-09 11:17:14 -0700

    Create dashboard route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+
+import Dashboard from './components/Dashboard';
+import Display from './components/Display';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<p>
-						Edit <code>src/App.js</code> and
-						save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
-			</div>
+			<Fragment>
+				<Route
+					path="/dashboard"
+					component={Dashboard}
+				/>
+			</Fragment>
 		);
 	}
 }
